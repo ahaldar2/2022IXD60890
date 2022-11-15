b0d43ce8c923a78e6407f01bf93abbcc8846a661
ahaldar2 <99573005+ahaldar2@users.noreply.github.com>
2022-11-15 13:21:09 -0800

    Add files via upload

---
+++ haldar_aindrila_m10/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<title> Store Home Page </title>
+	<? include "parts/meta.php"; ?>
+
+</head>
+<body>
+	<? include "parts/navbar.php"; ?>
+<div class="container">
+	<div class="card soft">
+
+		<h2>Home Page</h2>
+
+		<p>Create a home page here</p>
+			
+	</div>
+</div>
+
+</body>
+</html>
