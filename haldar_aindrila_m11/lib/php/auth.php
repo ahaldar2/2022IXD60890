bde72ead43f392c852ecf62e3e45dbb7e35f9012
ahaldar2 <99573005+ahaldar2@users.noreply.github.com>
2022-11-22 11:03:30 -0800

    Add files via upload

---
+++ haldar_aindrila_m11/lib/php/auth.php
@@ -0,0 +1,16 @@
+<?
+
+function MYSQLIAuth() {
+	return [
+
+		"localhost", // localhost from GoDaddy
+		"aindrila", // my database user name
+		"Aindrilaiscool", // database password
+		"ixd608_store" // database name
+
+	];
+}
+
+
+
+?>
